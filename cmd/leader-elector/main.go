39409f0471094c4c27f4a25d62d219b651e787e9
Kevin Kosmrli <kkosmrli@communicode.de>
2019-11-25 21:41:42 +0100

    Add leader handler

--- cmd/leader-elector/main.go
+++ cmd/leader-elector/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/kkosmrli/leader-elector/pkg/election"
 	"k8s.io/klog"
 )
 
-var leader string
+var leader Leader
+
+// Leader contains the name of the current leader of this election
+type Leader struct {
+	Name string `json:"name"`
+}
 
 func leaderHandler(res http.ResponseWriter, req *http.Request) {
-	// ToDo: Implement me
+	data, err := json.Marshal(leader)
+	if err != nil {
+		klog.Errorf("Error while marshaling leader response: %s", err.Error())
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res.Write(data)
 }
 
 func main() {
@@ -20,7 +32,7 @@ func main() {
 
 	callback := func(name string) {
 		klog.Infof("Currently leading: %s", name)
-		leader = name
+		leader = Leader{name}
 	}
 
 	go election.NewElection(ctx, callback)
